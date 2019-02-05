fffd41669ba11b2c4cfccb11fc617df0efb4595e
Rapteon <42870679+Rapteon@users.noreply.github.com>
2019-02-05 19:59:02 +0530

    C File to test new functions...

---
+++ try.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<windows.h>
+#define ANSI_COLOR_RED   "\x1b[31m"
+
+void colorit()
+{
+  HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+  CONSOLE_SCREEN_BUFFER_INFO consoleInfo;
+  WORD saved_attributes;
+
+  /* Save current attributes */
+  GetConsoleScreenBufferInfo(hConsole, &consoleInfo);
+   saved_attributes = consoleInfo.wAttributes;
+
+   SetConsoleTextAttribute(hConsole, FOREGROUND_GREEN);
+   printf("This is some nice COLORFUL text, isn't it?");
+
+   /* Restore original attributes */
+   SetConsoleTextAttribute(hConsole, saved_attributes);
+   printf("Back to normal");
+}
+
+
+int main()
+{
+  colorit();
+  return 0;
+}
